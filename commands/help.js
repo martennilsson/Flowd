0b94dab2573e2d16d63aeb632136285d99a0ee27
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-09-09 11:03:25 +0200

    Fix help command

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,11 @@
 exports.execute = function(args, callback){
-	callback("commands: "+  availableCommands.join(", "));
+  var commands = [];
+	var files = fs.readdirSync('commands');
+	for(var i=0; i<files.length; i++){
+		file = files[i];
+		if (file.match(/\.js$/)) {
+			commands.push(file.replace(/\.js$/, ""));
+    }
+  }
+  callback("Commands: "+  commands.join(", "));
 };
