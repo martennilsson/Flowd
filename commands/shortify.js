62c7b45e42508b3b1ea14ec99426a77d82a90acb
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-09-09 11:04:05 +0200

    Added shortify command

---
+++ commands/shortify.js
@@ -0,0 +1,12 @@
+http = require('http');
+exports.execute = function(url, callback) {
+  if (url) {
+    http.get({ host: 'is.gd', path: '/create.php?format=simple&url=' + url }, function(res) {
+      var data = '';
+      res.on('data', function(chunk) { data += chunk.toString(); });
+      res.on('end', function() { callback(data); });
+    });
+  } else {
+    callback('Please supply a URL!');
+  }
+}
