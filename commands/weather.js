00ddefb76faa9c495d18b39daffb2dbe987e7c40
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-09-09 11:52:37 +0200

    Added weather command

---
+++ commands/weather.js
@@ -0,0 +1,8 @@
+exports.execute = function(args, callback) {
+  jsdom.env("http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=ESSB", [
+  'http://code.jquery.com/jquery-1.5.min.js'
+  ], function(err, window){
+    var response = window.$('current_observation weather').text() + ", " + window.$('current_observation temp_c').text() + "°C, relative humidity: " + window.$('current_observation relative_humidity').text();
+    callback(response);
+  });
+}
