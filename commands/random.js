05e97329d1b00eac7b68911299f26e14f5fd053a
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-09-09 11:03:41 +0200

    Handle random without argument

--- commands/random.js
+++ commands/random.js
@@ -1,8 +1,10 @@
 exports.execute = function(args, callback){
-  if(isNaN(args)) {
+  if(args === "") {
+    callback("Please provide a max random number");
+  } else if(isNaN(args)) {
     callback("That's not a number!");
   } else {
-    callback(Math.round(Math.random()*args));
+    callback("" + Math.round(Math.random()*args));
   }
 };
 
